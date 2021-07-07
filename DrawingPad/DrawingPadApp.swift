f5848af23aaa0011c266f59b1574f22a4a096937
HY <pride508@gmail.com>
2021-07-07 22:57:06 +0800

    Initial Commit

---
+++ DrawingPad/DrawingPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingPadApp.swift
+//  DrawingPad
+//
+//  Created by HY Chuang on 2021/7/7.
+//
+
+import SwiftUI
+
+@main
+struct DrawingPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
